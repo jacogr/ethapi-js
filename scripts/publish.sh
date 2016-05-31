3133b11a0552aafe3e3a92057660a8139fd1af63
Jaco Greeff <jacogr@gmail.com>
2016-05-31 13:34:44 +0200

    Travis builds/publishes version

---
+++ scripts/publish.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -o errexit
+
+if [[ ("$TRAVIS_PULL_REQUEST" != "false") || ("$TRAVIS_BRANCH" != "master") ]]; then
+  exit 0
+fi
+
+git config --global user.email "admin@travis-ci.org"
+git config --global user.name "Travis CI"
+git config --global push.default simple
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
+git checkout master
+
+npm run build
+DATE=`date`
+echo "/* ${DATE} */" >> ./index.js
+git add --force index.js
+git commit --message "[CI skip] ${DATE}"
+
+npm version patch --message "[CI skip] ${DATE} %s"
+git push
+git push --tags

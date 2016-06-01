30792ecf27344fb07655236e707a06cfd32bb9b7
Jaco Greeff <jacogr@gmail.com>
2016-06-01 11:05:13 +0200

    publish package to npm

--- scripts/release.sh
+++ scripts/release.sh
@@ -13,12 +13,17 @@ git config credential.helper "store --file=.git/credentials"
 echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
 git checkout master
 
-npm run build
 DATE=`date`
+
+npm run build
 echo "/* ${DATE} */" >> ./index.js
+
 git add --force index.js
 git commit --message "[CI skip] ${DATE}"
 
 npm version patch --message "[CI skip] ${DATE} %s"
 git push
 git push --tags
+
+echo -e "$NPM_USERNAME\n$NPM_PASSWORD\n$NPM_EMAIL" | npm login
+npm publish

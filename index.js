cf863082a4ba728a30d2972a7f694a673363ce3f
Travis CI <admin@travis-ci.org>
2016-06-02 09:55:52 +0000

    [CI skip] Thu Jun  2 09:55:50 UTC 2016

--- index.js
+++ index.js
@@ -680,4 +680,4 @@ Contract = Contract;EthApi.
 Transports = { 
   JsonRpc: JsonRpc };
 
-module.exports = EthApi;/* Thu Jun  2 09:40:58 UTC 2016 */
+module.exports = EthApi;/* Thu Jun  2 09:55:50 UTC 2016 */

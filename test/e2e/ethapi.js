1b6ca0fdeb26920878ffcfcab19685c903e1d424
Jaco Greeff <jacogr@gmail.com>
2016-06-02 22:12:18 +0200

    use env to switch on RPC debug

--- test/e2e/ethapi.js
+++ test/e2e/ethapi.js
@@ -3,6 +3,8 @@ import EthApi from '../../lib';
 const transport = new EthApi.Transports.JsonRpc('127.0.0.1', 8545);
 const ethapi = new EthApi(transport);
 
-transport.setDebug(true);
+if (process.env.DEBUG) {
+  transport.setDebug(true);
+}
 
 export default ethapi;
